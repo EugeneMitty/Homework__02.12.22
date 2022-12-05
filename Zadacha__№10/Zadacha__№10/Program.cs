820153a61a764220f883c7cdb365072d0a1bfec0
Eugene <97278341+EugeneMtty@users.noreply.github.com>
2022-12-05 14:59:45 +0300

    Сделал

--- Zadacha__№10/Zadacha__№10/Program.cs
+++ Zadacha__№10/Zadacha__№10/Program.cs
@@ -1,2 +1,16 @@
-﻿Console.Write("Введите трех значное число");
-int number = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 999)
+{
+    Console.WriteLine("А ты точно умеешь считать?");
+}
+else if (num < 100)
+{
+    Console.WriteLine("Ты уверен?");
+}
+else
+{
+    int a = num % 100; int b =a / 10;
+    Console.WriteLine($"Вторая цифра {b}");
+}
