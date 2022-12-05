e781556fd7964d283e2f843b00b6ad9c3a19edf5
Eugene <97278341+EugeneMtty@users.noreply.github.com>
2022-12-05 20:03:47 +0300

    Нашли число

--- Zadacha__№13/Zadacha__№13/Program.cs
+++ Zadacha__№13/Zadacha__№13/Program.cs
@@ -1,3 +1,17 @@
-﻿Console.Write("Введите трех значное число");
+﻿Console.Write("Введите трех значное число ");
 int num = Convert.ToInt32(Console.ReadLine());
+int a = num;
+int result;
 
+if (num < 100)
+{
+    Console.WriteLine("Так, ты опять за свое?");
+}
+else
+{
+    for (num = a;
+        a > 1000;
+        a = a / 10) ;
+    result = a % 10;
+    Console.WriteLine($"Вот и наша цифра {result}");
+}
