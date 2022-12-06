957d47bee9724d5abe7c7fffd7d7c7e9bff8d4a7
Eugene <97278341+EugeneMtty@users.noreply.github.com>
2022-12-06 13:54:46 +0300

    Решение

---
+++ Zadacha__№15/Zadacha__№15/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите число ");
+int day = int.Parse(Console.ReadLine());
+
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Не выходной");
+}
